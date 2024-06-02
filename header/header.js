c458b8e710ff8f2af03b586f12901bac5ba7b77f
bigbutaa <betimcakiqiii@gmail.com>
2024-06-02 16:12:06 +0200

    modified header

--- header/header.js
+++ header/header.js
@@ -10,17 +10,18 @@ class HeaderComponent extends HTMLElement {
         <header>
         <nav>
             <div class="logoandnavbar">
-            <div class="logo">
-            <a href="index.html"><img src="../svg/logo.svg" alt=""></a>
-        </div>
-                <ul class="navlinks-items">
-
-                    <li><a href="index.html">Top offers</a></li>
-                    <li><a href="special_offers_page.html">Search in offers</a></li>
-                    <li><a href="#">References</a></li>
-                    <li><a href="#">About us</a></li>
-                    <li><a href="#">Our team</a></li>
-                </ul>
+                <div class="logo">
+                    <a href="index.html"><img src="../svg/logo.svg" alt=""></a>
+                </div>
+                <div class="navbar">
+                    <ul id="menuList">
+                        <li><a class="nav-link" href="index.html">Top offers</a></li>
+                        <li><a class="nav-link" href="special_offers_page.html">Search in offers</a></li>
+                        <li><a class="nav-link" href="#">References</a></li>
+                        <li><a class="nav-link" href="#">About us</a></li>
+                        <li><a class="nav-link" href="#">Our team</a></li>
+                    </ul>
+                </div>
             </div>
             <li id="contactus"><button><a href="tel:+38345384290">Contact us</a></button></li>
         </nav>
