fc007ad9d8f771f6532c67711f2d2e87787842b2
bigbutaa <betimcakiqiii@gmail.com>
2024-06-04 18:46:21 +0200

    added full our team grid slider for better user experience ;)

---
+++ script/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".sectiongrid", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
