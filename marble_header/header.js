f7ef984c14b604d4aa815478371359320b308d8b
bigbutaa <betimcakiqiii@gmail.com>
2024-06-05 23:15:15 +0200

    added responsive menu for mobile and added transitions to menu icons and menulist

--- marble_header/header.js
+++ marble_header/header.js
@@ -19,7 +19,7 @@ updateTitle(newTitle);
 // Favicon 
 
 // Define the path to the new favicon SVG file
-const newFaviconUrl = '../svg/logo.svg';
+const newFaviconUrl = '../marble_svg/logo.svg';
 
 // Function to update the favicon
 function updateFavicon() {
@@ -44,26 +44,34 @@ class HeaderComponent extends HTMLElement {
     render() {
         this.innerHTML =
             `
-        <link rel="stylesheet" href="../header/header.css">
+        <link rel="stylesheet" href="../marble_header/header.css">        
         <header>
-            <nav class="navigation-bar">
-                    <div class="logoandnavbar">
-                        <div class="logo">
-                            <a href="../pages/index.html#home"><img src="../svg/logo.svg" alt=""></a>
-                        </div>
-                        <div class="navbar">
-                            <ul id="menuList">
-                                <li><a class="nav-link" href="../pages/index.html#top-offer">Top offers</a></li>
-                                <li><a class="nav-link" href="../pages/special_offers_page.html">Search in offers</a></li>
-                                <li><a class="nav-link" href="../pages/index.html#references">References</a></li>
-                                <li><a class="nav-link" href="../pages/index.html#aboutus">About us</a></li>
-                                <li><a class="nav-link" href="../pages/index.html#ourteam">Our team</a></li>
-                                <li id="contactus"><button><a href="tel:+38345384290">Contact us</a></button></li>
-                            </ul>
-                        </div>
-                    </div>
+        <nav class="navigation-bar">
+            <div class="logoandnavbar">
+                <div class="logo">
+                    <a href="../marble_pages/index.html#home"><img src="../marble_svg/logo.svg" alt=""></a>
                 </div>
-            </nav>
+                <div class="navbar">
+                    <ul id="menuList">
+                        <li><a class="nav-link" href="../marble_pages/index.html#top-offer">Top offers</a></li>
+                        <li><a class="nav-link" href="../marble_pages/special_offers_page.html">Search in offers</a></li>
+                        <li><a class="nav-link" href="../marble_pages/index.html#references">References</a></li>
+                        <li><a class="nav-link" href="../marble_pages/index.html#aboutus">About us</a></li>
+                        <li><a class="nav-link" href="../marble_pages/index.html#ourteam">Our team</a></li>
+                        <li id="contactus"><button><a href="tel:+38345384290">Contact us</a></button></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="burgerMenu" onclick="handleBurgerMenuAction()">
+                <div id="menuIcon">
+                    <img src="../marble_svg/burger-menu-icon.svg" alt="">
+                </div>
+                <div id="closeIcon">
+                    <img src="../marble_svg/burger-menu-close-icon.svg" alt="">
+                </div>
+            </div>
+            </div>
+        </nav>
     </header>
         `
     }
@@ -87,7 +95,7 @@ class FooterComponent extends HTMLElement {
         <div class="sub-newsletter-footer">
             <div class="sub-newsletter-title-description">
 
-                <img class="sub-newsletter-icon-svg" src="../svg/news-letter-icon.svg" alt="">
+                <img class="sub-newsletter-icon-svg" src="../marble_svg/news-letter-icon.svg" alt="">
 
                 <div class="sub-news-letter-without-img">
                     <div class="sub-news-letter-title-text">
@@ -115,7 +123,7 @@ class FooterComponent extends HTMLElement {
         <div class="copyright-marble-footer">
             <div class="copyright-marble-footer-full-container">
                 <div class="copyright-marble-footer-container">
-                        <img class="copyrigt-marble-footer-logo" src="../svg/footer-logo.svg" alt="">
+                        <img class="copyrigt-marble-footer-logo" src="../marble_svg/footer-logo.svg" alt="">
 
                         <p class="copyrigt-marble-footer-name">
                             ©2024 Marble Properties
@@ -130,3 +138,35 @@ class FooterComponent extends HTMLElement {
 }
 customElements.define('custom-footer', FooterComponent)
 
+
+
+
+const menuListElement = document.getElementById('menuList');
+const menuIconElement = document.getElementById('menuIcon');
+const closeIconElement = document.getElementById('closeIcon');
+
+const handleBurgerMenuAction = () => {
+    const prevState = menuListElement.style.maxHeight;
+    const isBurgerMenuOpened = prevState === '290px';
+
+    // Check if the menu icon is visible based on media screen size
+    const isMenuIconVisible = window.matchMedia("(max-width: 1166px)").matches;
+
+    if (isBurgerMenuOpened) {
+        menuListElement.style.maxHeight = '0px';
+        if (isMenuIconVisible) {
+            menuIconElement.style.transform = 'translateX(100%) scale(1) rotateY(0deg)';
+            menuIconElement.style.opacity = '1';
+            closeIconElement.style.transform = 'translateX(100%) scale(0) rotateY(180deg)';
+            closeIconElement.style.opacity = '0';
+        }
+    } else {
+        menuListElement.style.maxHeight = '290px';
+        if (isMenuIconVisible) {
+            menuIconElement.style.transform = 'translateX(100%) scale(0) rotateY(180deg)';
+            menuIconElement.style.opacity = '0';
+            closeIconElement.style.transform = 'translateX(100%) scale(1) rotateY(0deg)';
+            closeIconElement.style.opacity = '1';
+        }
+    }
+};
