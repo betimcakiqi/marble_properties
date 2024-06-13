1b74ad6f542c5d6c8faff9970c66483d5c5cc2e9
bigbutaa <betimcakiqiii@gmail.com>
2024-06-13 11:21:08 +0200

    price range space between thumbs

--- components/sliders/price-range-special-offers/price-range.js
+++ components/sliders/price-range-special-offers/price-range.js
@@ -1,6 +1,6 @@
 const rangevalue = document.querySelector(".slider-container .price-slider");
 const rangeInputvalue = document.querySelectorAll(".range-input input");
-let priceGap = 40000;
+let priceGap = 45000;
 
 const priceInputvalue = document.querySelectorAll(".price-input input");
 
