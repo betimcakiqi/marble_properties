7c64b4c7ba1b97772919c04927d3f3ed0ecba299
bigbutaa <betimcakiqiii@gmail.com>
2024-06-13 10:56:09 +0200

    price range fixed not clipping anymore

--- components/sliders/price-range-special-offers/price-range.js
+++ components/sliders/price-range-special-offers/price-range.js
@@ -1,6 +1,6 @@
 const rangevalue = document.querySelector(".slider-container .price-slider");
 const rangeInputvalue = document.querySelectorAll(".range-input input");
-let priceGap = 40500;
+let priceGap = 40000;
 
 const priceInputvalue = document.querySelectorAll(".price-input input");
 
