c7afebb01d52592150f165cbf61435a8ce0a66c7
bigbutaa <betimcakiqiii@gmail.com>
2024-06-13 13:54:02 +0200

    # to email addres

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -24,7 +24,7 @@ class FooterComponent extends HTMLElement {
                         </span>
                     </div>
 
-                    <form action="" method="post" class="sub-news-letter-form-container">
+                    <form action="#" method="post" class="sub-news-letter-form-container">
                         <div class="sub-news-letter-email-address-container">
                             <input id="sub-news-letter-email-address-bar" type="email" name="email-addres"
                                 placeholder="Your e-mail address" required>
