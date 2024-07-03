4dcbf3bfed3ce918e86f525378dff431f9b5e684
bigbutaa <betimcakiqiii@gmail.com>
2024-07-04 00:25:01 +0200

    changed pic,title,price,city for each product

--- pages/special-offers_page/product/special_offers_product.js
+++ pages/special-offers_page/product/special_offers_product.js
@@ -1,172 +1,172 @@
 // Define Products Data
 const productsData = [
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/malibu_beachhouse/cover.jpg",
+        "title": "Luxury Beach House in Malibu with Stunning Ocean Views",
+        "price": "10 500 000€",
+        "city": "Malibu, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/beverly_hills_modern_villa/cover.jpg",
+        "title": "Modern Villa in Beverly Hills with Infinity Pool",
+        "price": "15 000 000€",
+        "city": "Beverly Hills, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/manhattan_penthouse_central_park/cover.jpg",
+        "title": "Penthouse in Manhattan Overlooking Central Park",
+        "price": "22 000 000€",
+        "city": "New York, New York"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/hamptons_estate/cover.jpg",
+        "title": "Charming Estate in the Heart of the Hamptons",
+        "price": "8 750 000€",
+        "city": "The Hamptons, New York"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/san_francisco_contemporary_home/cover.jpg",
+        "title": "Contemporary Home in San Francisco with Bay Views",
+        "price": "12 300 000€",
+        "city": "San Francisco, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/boston_beacon_hill_mansion/cover.jpg",
+        "title": "Historic Mansion in Boston's Beacon Hill",
+        "price": "9 600 000€",
+        "city": "Boston, Massachusetts"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/aspen_mountain_retreat/cover.jpg",
+        "title": "Secluded Retreat in Aspen with Mountain Views",
+        "price": "18 200 000€",
+        "city": "Aspen, Colorado"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/miami_beach_luxury_condo/cover.jpg",
+        "title": "Luxury Condo in Miami Beach with Private Terrace",
+        "price": "11 500 000€",
+        "city": "Miami Beach, Florida"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/dallas_ranch/cover.jpg",
+        "title": "Spacious Ranch in Dallas with Extensive Land",
+        "price": "7 800 000€",
+        "city": "Dallas, Texas"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/chicago_gold_coast_apartment/cover.jpg",
+        "title": "Lavish Apartment in Chicago's Gold Coast",
+        "price": "6 400 000€",
+        "city": "Chicago, Illinois"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/santa_barbara_ocean_villa/cover.jpg",
+        "title": "Stunning Villa in Santa Barbara with Ocean Views",
+        "price": "13 250 000€",
+        "city": "Santa Barbara, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/georgetown_townhouse/cover.jpg",
+        "title": "Elegant Townhouse in Georgetown",
+        "price": "8 300 000€",
+        "city": "Washington, D.C."
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/seattle_waterfront_home/cover.jpg",
+        "title": "Waterfront Home in Seattle with Private Dock",
+        "price": "14 750 000€",
+        "city": "Seattle, Washington"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/montana_sprawling_ranch/cover.jpg",
+        "title": "Sprawling Ranch in Montana with Mountain Views",
+        "price": "12 100 000€",
+        "city": "Bozeman, Montana"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/los_angeles_downtown_loft/cover.jpg",
+        "title": "Modern Loft in Downtown Los Angeles",
+        "price": "6 900 000€",
+        "city": "Los Angeles, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/brooklyn_heights_brownstone/cover.jpg",
+        "title": "Classic Brownstone in Brooklyn Heights",
+        "price": "8 700 000€",
+        "city": "Brooklyn, New York"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/vail_luxury_chalet/cover.jpg",
+        "title": "Luxury Chalet in Vail with Ski-In/Ski-Out Access",
+        "price": "17 000 000€",
+        "city": "Vail, Colorado"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/honolulu_beachfront_villa/cover.jpg",
+        "title": "Beachfront Villa in Honolulu with Private Beach",
+        "price": "25 000 000€",
+        "city": "Honolulu, Hawaii"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/savannah_historic_estate/cover.jpg",
+        "title": "Historic Estate in Savannah with Extensive Gardens",
+        "price": "5 500 000€",
+        "city": "Savannah, Georgia"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/san_diego_contemporary_penthouse/cover.jpg",
+        "title": "Contemporary Penthouse in San Diego",
+        "price": "9 900 000€",
+        "city": "San Diego, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/lake_tahoe_luxury_cabin/cover.jpg",
+        "title": "Luxury Cabin in Lake Tahoe with Lake Views",
+        "price": "11 200 000€",
+        "city": "Lake Tahoe, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/palm_beach_opulent_residence/cover.jpg",
+        "title": "Opulent Residence in Palm Beach with Ocean Access",
+        "price": "20 000 000€",
+        "city": "Palm Beach, Florida"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/napa_valley_estate_vineyard/cover.jpg",
+        "title": "Estate in Napa Valley with Vineyard",
+        "price": "16 500 000€",
+        "city": "Napa, California"
     },
     {
-        href: "./x_offer_1_page.html",
-        imgSrc: "./assets/properties/malibu_beachhouse/cover.jpg",
-        title: "Luxury Beach House in Malibu with Stunning Ocean Views",
-        price: "10 500 000€",
-        city: "Malibu, California"
+        "href": "./x_offer_1_page.html",
+        "imgSrc": "./assets/properties/las_vegas_exclusive_mansion/cover.jpg",
+        "title": "Exclusive Mansion in Las Vegas with Strip Views",
+        "price": "14 800 000€",
+        "city": "Las Vegas, Nevada"
     },
 ];
 
