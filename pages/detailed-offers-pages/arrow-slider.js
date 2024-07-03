10d52d3870943cac09f939dcdf0853eb1ffaf7c1
bigbutaa <betimcakiqiii@gmail.com>
2024-07-03 02:19:02 +0200

    when screen is under 900px it shows only 2 slides per view

--- pages/detailed-offers-pages/arrow-slider.js
+++ pages/detailed-offers-pages/arrow-slider.js
@@ -1,11 +1,36 @@
-var swiper = new Swiper(".detailed-offer-small-img-slider", {
-  slidesPerView: 3,
-  spaceBetween: 0,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+var swiper;
+
+function initSwiper() {
+  var screenWidth = window.innerWidth;
+  var slidesPerView;
+
+  if (screenWidth >= 900) {
+    slidesPerView = 3;
+  } else {
+    slidesPerView = 2;
+  }
+
+  swiper = new Swiper(".detailed-offer-small-img-slider", {
+    slidesPerView: slidesPerView,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+// Initialize Swiper on page load
+initSwiper();
+
+// Update Swiper on window resize
+window.addEventListener('resize', function() {
+  if (swiper) {
+    swiper.destroy(); // Destroy Swiper instance
+  }
+  initSwiper(); // Reinitialize Swiper with updated configuration
 });
+
 
 // CONTACT FORM TELEPHONE
 
